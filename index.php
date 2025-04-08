dea0af6392e9d674890ea705d7e8dc020390ef79
Mels Sargsyan <melssargsyan1@gmail.com>
2025-04-08 16:21:56 +0400

    Successfully Added-1

--- index.php
+++ index.php
@@ -14,9 +14,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
-
-<>
-
     <section id="header">
         <a href="#"><img src="images/redstore.png" alt="Redstore Image" class="logo"></a>
 
